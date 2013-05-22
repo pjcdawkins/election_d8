bd0553620e62e34df246fbc507994f4b02c6cc70
Patrick Dawkins <pjcdawkins@gmail.com>
2013-05-22 18:47:49 +0100

    Core ControllerInterface has a new namespace.

--- lib/Drupal/election/ElectionController.php
+++ lib/Drupal/election/ElectionController.php
@@ -6,10 +6,9 @@
  */
 namespace Drupal\election;
 
+use Drupal\Core\Controller\ControllerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-
-use Drupal\Core\ControllerInterface;
 
 /**
  * Controller for core Election routes.
@@ -20,14 +19,15 @@ class ElectionController implements ControllerInterface {
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container) {
-     return new static();
+    return new static();
   }
 
   /**
    * Content for the path '/elections'.
    */
   public function overviewList(Request $request) {
-     return 'Hello, world!<br />This will be a list of elections.';
+    drupal_set_title(t('Elections'), PASS_THROUGH);
+    return 'Hello, world!<br />This will be a list of elections.';
   }
 
 }
